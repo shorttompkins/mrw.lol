4621852ddb069691b2a68105d79042a4e467200a
Jason Krol <jkat98@gmail.com>
2015-10-12 13:37:21 -0400

    Force eslint to stop throwing errors on unused React

--- client/js/main.js
+++ client/js/main.js
@@ -4,7 +4,6 @@ import routes from './routes'
 
 window.React = React
 
-// HistoryLocation === html5mode
 Router.run(routes, Router.HistoryLocation, function(Root, state){
   React.render(<Root {...state}/>, document.getElementById('app'))
 })
