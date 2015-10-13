c26805620e1d2072a5e861612a9a52e9671a9559
Jason Krol <jkat98@gmail.com>
2015-10-13 18:53:58 -0400

    Include Gulp SASS support
    And beginning styles for the app.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     browserify = require('browserify'),
     babelify = require('babelify'),
+    sass = require('gulp-sass'),
     del = require('del'),
     //nodemon = require('gulp-nodemon'),
     source = require('vinyl-source-stream')
@@ -12,6 +13,12 @@ gulp.task('clean', function(cb) {
 gulp.task('copy', function() {
   return gulp.src(['client/img/**/*'])
     .pipe(gulp.dest('server/public/apps/blog/img'))
+})
+
+gulp.task('sass', function () {
+  gulp.src('client/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('server/public/apps/blog/'))
 })
 
 gulp.task('transform', function () {
@@ -26,8 +33,9 @@ gulp.task('transform', function () {
   .pipe(gulp.dest('server/public/apps/blog'))
 })
 
-gulp.task('default', ['clean', 'copy', 'transform'], function() {
+gulp.task('default', ['clean', 'copy', 'transform', 'sass'], function() {
   gulp.watch('client/**/*', ['transform'])
+  gulp.watch('client/sass/**/*.scss', ['sass'])
 })
 
 gulp.task('build', ['clean', 'copy', 'transform'])
