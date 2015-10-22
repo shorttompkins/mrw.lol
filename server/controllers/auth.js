b95e18d2e4c5f62b467b0474ed60ad9d9fc363ad
Jason Krol <jkat98@gmail.com>
2015-10-22 15:35:20 -0400

    ctrl: Passport authentication controller and network specific routes

---
+++ server/controllers/auth.js
@@ -0,0 +1,57 @@
+let passport = require('passport'),
+    Models = require('../models'),
+    config = require('../config/auth'),
+    passport_strategies = {
+      github: require('passport-github2').Strategy,
+      facebook: require('passport-facebook').Strategy,
+      twitter: require('passport-twitter').Strategy,
+      google: require('passport-google-oauth').OAuth2Strategy
+    }
+
+let handler = (accessToken, refreshToken, profile, done) => {
+  let uniqueVal
+
+  switch(profile.provider) {
+    case 'facebook':
+      uniqueVal = profile.emails[0].value
+      break
+    case 'twitter':
+      uniqueVal = profile.username
+      break
+    case 'google':
+      uniqueVal = profile.emails[0].value
+      break
+    case 'github':
+      uniqueVal = profile.emails[0].value
+      break
+  }
+
+  let searchQuery = { uniqueVal: uniqueVal },
+      updates = { name: profile.displayName, networkID: profile.id, provider: profile.provider, uniqueVal: uniqueVal, accessToken: accessToken },
+      options = { upsert: true }
+
+  Models.User.findOneAndUpdate(searchQuery, updates, options, (err, user) => {
+    if (err) {
+      return done(err)
+    } else {
+      return done(null, user)
+    }
+  })
+}
+
+function init() {
+  passport.serializeUser(function(user, done) {
+    done(null, user.id)
+  })
+  passport.deserializeUser(function(id, done) {
+    Models.User.findById(id, function (err, user) {
+      done(err, user)
+    })
+  })
+}
+
+module.exports = function(strategy) {
+  passport.use(new passport_strategies[strategy](config[strategy], handler))
+  init()
+  return passport
+}
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 var controllers = require('./controllers'),
-    api = require('./controllers/api')
+    api = require('./controllers/api'),
+    auth = require('./controllers/auth.js')
 
 module.exports.initialize = function(app, router) {
 
@@ -10,6 +11,28 @@ module.exports.initialize = function(app, router) {
 
   // ** APPS **
   router.get('/apps/blog*', controllers.apps.blog)
+
+  // ** AUTHENTICATION **
+  function authHandler(req, res) {
+    req.session.user_id = req.user._id
+    res.redirect('/')
+  }
+  router.get('/auth/twitter', auth('twitter').authenticate('twitter'))
+  router.get('/auth/twitter/callback', auth('twitter').authenticate('twitter', { failureRedirect: '/' }), authHandler)
+
+  router.get('/auth/facebook', auth('facebook').authenticate('facebook', { scope: 'email' }))
+  router.get('/auth/facebook/callback', auth('facebook').authenticate('facebook', { failureRedirect: '/' }), authHandler)
+
+  router.get('/auth/github', auth('github').authenticate('github', { scope: [ 'user:email' ] }))
+  router.get('/auth/github/callback', auth('github').authenticate('github', { failureRedirect: '/' }), authHandler)
+
+  router.get('/auth/google', auth('google').authenticate('google', { scope: [ 'profile', 'email' ] }))
+  router.get('/auth/google/callback', auth('google').authenticate('google', { failureRedirect: '/' }), authHandler)
+
+  router.get('/auth/logout', (req, res) => {
+    req.session.destroy()
+    res.redirect('/')
+  })
 
 
   // ** API **
