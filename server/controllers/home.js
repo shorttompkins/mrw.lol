0edc8db6c1bde2e05f1ed8a3fa08f0221cef8b98
Jason Krol <jkat98@gmail.com>
2015-10-22 15:35:57 -0400

    Show login nav and user login status

--- server/controllers/home.js
+++ server/controllers/home.js
@@ -1,11 +1,14 @@
 module.exports = {
   index(req, res) {
-
+    let loggedin = req.session.user_id || false
+    
     res.render('home', {
       title: 'World',
       session_id: req.session.id,
       testing: req.session.testing,
-      version: req.app.get('config').version
+      version: req.app.get('config').version,
+      loggedin: loggedin,
+      userid: req.session.user_id
     })
   }
 
