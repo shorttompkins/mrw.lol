5a64826b34e02c02c17fed0b722fc8f1948e6e10
Jason Krol <jkat98@gmail.com>
2015-11-10 14:13:43 -0500

    Cleanup image Card component

--- client/js/components/images/Card.js
+++ client/js/components/images/Card.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router'
 
-const Card = ({image}) => (
-  <span key={image._id}>
-    <Link to={`/image/${encodeURIComponent(image.filename)}`}>
-      <img src={image.url} />
-    </Link>
-  </span>
-)
+const Card = ({image}) => {
+  let styles = {
+    'backgroundImage': `url(${image.url})`
+  }
+  return (
+    <span key={image._id}>
+      <Link to={`/image/${encodeURIComponent(image.filename)}`}>
+        <div className="image-card" style={styles}></div>
+      </Link>
+    </span>
+  )
+}
 
 export default Card
