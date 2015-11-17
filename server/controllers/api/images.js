e9d35263037f15d5c70bf362968ff786a4de6161
Jason Krol <jkat98@gmail.com>
2015-11-16 23:01:05 -0500

    Update Image model
    Include virtual property for tags and statics to loadTags and generateUniqueId

--- server/controllers/api/images.js
+++ server/controllers/api/images.js
@@ -2,27 +2,17 @@ let Models = require('../../models'),
     ObjectId = require('mongoose').Types.ObjectId
 
 let add_user_image = function(tags, tag, image_id) {
-      let tagged = false
-      for(let t = 0; t < tags.length; t += 1) {
-        if (tags[t].name === tag) {
-          tagged = true
-          tags[t].images.push(image_id)
-        }
-      }
-      if (!tagged) {
-        tags.push({name: tag, images: [image_id]})
-      }
-    },
-    generateUniqueid = function() {
-      let length = 16,
-          charset = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
-          retVal = ''
-
-      for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n))
-      }
-      return retVal
+  let tagged = false
+  for(let t = 0; t < tags.length; t += 1) {
+    if (tags[t].name === tag) {
+      tagged = true
+      tags[t].images.push(image_id)
     }
+  }
+  if (!tagged) {
+    tags.push({name: tag, images: [image_id]})
+  }
+}
 
 module.exports = {
   list(req, res) {
@@ -91,22 +81,25 @@ module.exports = {
 
   getImageById(req, res) {
     Models.Image.findOne({_id: req.params.id}, (err, image) => {
-      // to do: would be nice to append list of tags associated with this image
-      res.json(image)
+      Models.Image.getTags(image, () => {
+        res.json(image)
+      })
     })
   },
 
   getImageByUniqueId(req, res) {
     Models.Image.findOne({uniqueid: req.params.uniqueid}, (err, image) => {
-      // to do: would be nice to append list of tags associated with this image
-      res.json(image)
+      Models.Image.getTags(image, () => {
+        res.json(image)
+      })
     })
   },
 
   getImageByFilename(req, res) {
     Models.Image.findOne({filename: encodeURIComponent(req.params.filename.toLowerCase())}, (err, image) => {
-      // to do: would be nice to append list of tags associated with this image
-      res.json(image)
+      Models.Image.getTags(image, () => {
+        res.json(image)
+      })
     })
   },
 
@@ -116,7 +109,7 @@ module.exports = {
         Models.Image.create({
           url: req.body.url,
           filename: req.body.url.substring(req.body.url.lastIndexOf('/')+1),
-          uniqueid: generateUniqueid()
+          uniqueid: Models.Image.generateUniqueId()
         }, (err, image) => {
           req.body.tags.forEach(tag => {
             add_user_image(user.tags, tag, image._id)
@@ -148,7 +141,8 @@ module.exports = {
 
         Models.Image.create({
           url: 'http://shorttompkins.js.org/gifs/ah%20ah%20ah%20didnt%20say%20the%20magic%20word.gif',
-          filename: 'ah%20ah%20ah%20didnt%20say%20the%20magic%20word.gif'
+          filename: 'ah%20ah%20ah%20didnt%20say%20the%20magic%20word.gif',
+          uniqueid: Models.Image.generateUniqueId()
         }, (err, image) => {
           add_user_image(user.tags, 'nope', image._id)
           add_user_image(user.tags, 'no', image._id)
@@ -159,7 +153,8 @@ module.exports = {
 
         Models.Image.create({
           url: 'http://shorttompkins.js.org/gifs/omnomnomcat.gif',
-          filename: 'omnomnomcat.gif'
+          filename: 'omnomnomcat.gif',
+          uniqueid: Models.Image.generateUniqueId()
         }, (err, image) => {
           add_user_image(user.tags, 'yum', image._id)
           add_user_image(user.tags, 'nomnom', image._id)
@@ -170,7 +165,8 @@ module.exports = {
 
         Models.Image.create({
           url: 'http://shorttompkins.js.org/gifs/nonono.gif',
-          filename: 'nonono.gif'
+          filename: 'nonono.gif',
+          uniqueid: Models.Image.generateUniqueId()
         }, (err, image) => {
           add_user_image(user.tags, 'nope', image._id)
           add_user_image(user.tags, 'no', image._id)
@@ -180,7 +176,8 @@ module.exports = {
 
         Models.Image.create({
           url: 'http://shorttompkins.js.org/gifs/owned_by_goat.jpg',
-          filename: 'owned_by_goat.jpg'
+          filename: 'owned_by_goat.jpg',
+          uniqueid: Models.Image.generateUniqueId()
         }, (err, image) => {
           add_user_image(user.tags, 'owned', image._id)
           add_user_image(user.tags, 'ouch', image._id)
@@ -190,7 +187,8 @@ module.exports = {
 
         Models.Image.create({
           url: 'http://shorttompkins.js.org/gifs/dissappointed_panda.gif',
-          filename: 'dissappointed_panda.gif'
+          filename: 'dissappointed_panda.gif',
+          uniqueid: Models.Image.generateUniqueId()
         }, (err, image) => {
           add_user_image(user.tags, 'facepalm', image._id)
           add_user_image(user.tags, 'dissappointed', image._id)
--- server/models/image.js
+++ server/models/image.js
@@ -5,6 +5,37 @@ let ImageSchema = new Schema({
   url:        String,
   filename:   String,
   uniqueid:   String
-})
+}, { toJSON: { virtuals: true } })
+
+ImageSchema.virtual('tags')
+  .set((tags) => {
+    this._tags = tags
+  })
+  .get(() => this._tags)
+
+ImageSchema.statics.generateUniqueId = function() {
+  let length = 16,
+      charset = 'abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+      retVal = ''
+
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    retVal += charset.charAt(Math.floor(Math.random() * n))
+  }
+  return retVal
+}
+
+ImageSchema.statics.getTags = function(image, cb) {
+  let UserModel = require('./user')
+  UserModel.aggregate([
+    { $unwind: '$tags' },
+    { $sort: { 'tags.name': -1 } },
+    { $unwind: '$tags.images' },
+    { $group: { '_id': '$tags.images', 'tags': { $addToSet: '$tags.name' } } },
+    { $match: { '_id': image._id } }
+  ], (err, tags) => {
+    image.tags = tags[0].tags
+    cb()
+  })
+}
 
 module.exports = mongoose.model('Image', ImageSchema)
