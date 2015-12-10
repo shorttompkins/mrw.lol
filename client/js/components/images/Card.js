0b86841ea3948ddaa7d02f5163a360fe33f03d6e
Jason Krol <jkat98@gmail.com>
2015-12-10 16:39:46 -0500

    Pause gifs by default, animate on hover
    Using canvas, only display the first frame of every gif by default.  On hover, swap
    the canvas with the actual gif so it animates.

--- client/js/components/images/Card.js
+++ client/js/components/images/Card.js
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-const Card = ({image}) => {
-  let styles = {
-    'backgroundImage': `url(${image.web_url})`
+class Card extends Component {
+  static propTypes = {
+    image: React.PropTypes.object
   }
-  return (
-    <span key={image._id}>
-      <Link to={`/image/${image.uniqueid}`}>
-        <div className="image-card" style={styles}>
-          <i className="fa fa-search-plus"></i>
-        </div>
-      </Link>
-    </span>
-  )
+
+  hoverHandler = () => {
+    let canvas = this.refs.canvas,
+        image = this.refs.image
+
+    if (canvas.className === 'show') {
+      image.className = 'show'
+      canvas.className = 'hide'
+    } else {
+      image.className = 'hide'
+      canvas.getContext('2d').drawImage(image, 0, 0, 190, 190)
+      canvas.className = 'show'
+    }
+  }
+
+  gifHandler = () => {
+    let canvas = this.refs.canvas,
+        image = this.refs.image
+
+    const ctx = canvas.getContext('2d')
+    ctx.drawImage(image, 0, 0, 190, 190)
+  }
+
+  render() {
+    let { image } = this.props
+    return (
+      <span key={image._id}>
+        <Link to={`/image/${image.uniqueid}`}>
+          <div className="image-card">
+            <i className="fa fa-search-plus"></i>
+            <img ref="image" src={image.web_url} onLoad={this.gifHandler} className="hide" onMouseOut={this.hoverHandler} /><br/>
+            <canvas ref="canvas" width="190" height="190" className="show" onMouseOver={this.hoverHandler} />
+          </div>
+        </Link>
+      </span>
+    )
+  }
 }
 
 export default Card
