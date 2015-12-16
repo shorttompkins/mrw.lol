e03f627bc5f57937f195cec07138cc23331fe256
Jason Krol <jkat98@gmail.com>
2015-12-16 16:17:00 -0500

    Tag component
    Reusable component for display a tag (normal and/or user)

---
+++ client/js/components/images/Tag.js
@@ -0,0 +1,32 @@
+import React, { Component, PropTypes } from 'react'
+import { Link } from 'react-router'
+
+class Tag extends Component {
+  static propTypes = {
+    tag: PropTypes.string.isRequired,
+    userid: PropTypes.string
+  }
+
+  static defaultProps = {
+    userid: ''
+  }
+  
+  render() {
+    let { tag } = this.props
+
+    let className = 'tag-link',
+        linkUrl = `/images/${tag}`
+    if (this.props.userid !== '') {
+      className += ' user-tag'
+      linkUrl = `/users/${this.props.userid}/${tag}`
+    }
+
+    return (
+      <Link className={className} to={linkUrl}>
+        <i className="fa fa-tag"></i> {tag}
+      </Link>
+    )
+  }
+}
+
+export default Tag
