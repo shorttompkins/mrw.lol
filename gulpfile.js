5ceb60aa244dd09cf0566a5179d92820969a8ce8
Jason Krol <jkat98@gmail.com>
2015-12-28 17:40:15 -0500

    Misc cleanup
    Housekeeping.

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,7 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     babelify = require('babelify'),
     sass = require('gulp-sass'),
-    del = require('del'),
-    //nodemon = require('gulp-nodemon'),
     source = require('vinyl-source-stream')
-
-gulp.task('clean', function(cb) {
-  del(['server/public/mrw/**'], cb)
-})
 
 gulp.task('copy', function() {
   return gulp.src(['client/img/**/*'])
@@ -33,9 +27,9 @@ gulp.task('transform', function () {
   .pipe(gulp.dest('server/public/mrw'))
 })
 
-gulp.task('default', ['clean', 'copy', 'transform', 'sass'], function() {
+gulp.task('default', ['copy', 'sass', 'transform'], function() {
   gulp.watch('client/**/*', ['transform'])
   gulp.watch('client/sass/**/*.scss', ['sass'])
 })
 
-gulp.task('build', ['clean', 'copy', 'sass', 'transform'])
+gulp.task('build', ['copy', 'sass', 'transform'])
